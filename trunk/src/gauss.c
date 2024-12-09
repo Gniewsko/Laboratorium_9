96b7adb4b58b93389ddb4ef51f7e93dd1ff2c344
Anastasiia Dmytrenko <01200577@pw.edu.pl>
2024-12-09 17:27:25 +0100

    Zmieniono plik gauss.c

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -4,10 +4,24 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+
+int eliminate(Matrix *mat, Matrix *b) {
+    int i, j, k;
+    int n = mat->r;
+
+    for (k = 0; k < n; k++) {
+        if (mat->data[k][k] == 0.0) {
+            return 1; // Dzielenie przez zero
+        }
+
+        for (i = k + 1; i < n; i++) {
+            double factor = mat->data[i][k] / mat->data[k][k];
+            for (j = k; j < n; j++) {
+                mat->data[i][j] -= factor * mat->data[k][j];
+            }
+            b->data[i][0] -= factor * b->data[k][0];
+        }
+    }
 
 		return 0;
 }
